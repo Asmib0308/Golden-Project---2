4735db3c641defd57e9ae1ca838be58cf76154e0
Asmib0308 <78547003+Asmib0308@users.noreply.github.com>
2021-09-04 23:52:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 var canvas, backgroundImage;
-
 var gameState = 0;
 var playerCount;
 var qid =1
 var form;
 var answer;
-
+var playerExists
 var form2;
-var rules;
 var title,guru;
 var hero, heroin, song, movie;
 var points = 30;
 var db
+var id
 
-var flag = 0;
+var userId 
+
+var answerFlag = 0;
 
 function preload(){
   gameback = loadImage("img/4.png")
@@ -30,9 +31,6 @@ function setup(){
   canvas = createCanvas(windowWidth, windowHeight);
   db = firebase.firestore()
   form = new Form()
-  //settings = new Settings()
-  //rules = new Rules()
-
 
   title = createSprite(windowWidth/2 + 20 , windowHeight/2 - 230)
   title.addImage(titleI)
@@ -69,47 +67,93 @@ function draw(){
   stroke(204, 36, 117)
   strokeWeight(1)
   fill(204, 36, 117)
-  text("Bolly coins : " + points,windowWidth/2 + 570, windowHeight/2 - 360)
-
+  text("Bolly coins : " + points,width/2 + 570, height/2 - 360)
+  
   if (gameState === 0){
     form.display()
-    line(windowWidth/2, windowHeight/2 - 200, windowWidth/2, windowHeight/2 - 30)
-    line(windowWidth/2 - 250, windowHeight/2 - 120, windowWidth/2 + 290, windowHeight/2 - 120)
-   }  
-  
-   if(gameState === 1 ){
-    
+    line(width/2, height/2 - 220, width/2, height/2 - 30)
+    line(width/2 - 270, height/2 - 120, width/2 + 310, height/2 - 120)
+  }  
+
+  if(gameState === 1 ){
+    form.hide()
     form2.display()
     form2.getDataHints()
-    form2.displayQuestions()
-    
+    form2.displayQuestions()    
   }
-  if (flag === 4){
-    flag = 0;
+
+  if (answerFlag === 4){
+    answerFlag = 0;
     reset();
-
   }
 
-  if (gameState === 3){   
-  //settings.display()
+  if(playerExists === false){
+    db.collection("Players").add({email:form.input.value(),q_id:qid})
+    playerExists = true
   }
 
-  if (gameState === 4){   
-//  rules.display()
-  }
-  
   drawSprites();
 }
+
 function reset(){
   form2.show();
   points += 20;
   qid += 1;
-  form2.clickFlag = 0;
-  form2.updatePlayers(qid)
+  form2.clickFlag = 0
 }
-function keyPressed(){ 
-  if(keyCode === 13 ){
-     //form.createPlayer() 
-     form.addPlayer() 
+
+function keyPressed(){
+  if(keyCode === 13 && gameState === 0){
+    userId = form.input.value()
+    if(userId!==''){
+      console.log(userId)
+      if(form.validation(userId)){
+ 
+        checkPlayer(userId)
+        if(playerExists!== undefined){
+          addPlayer()
+        }
+      }  
+    }
+    else{
+      alert('enter email to continue')
+    }
   }
+}
+
+async  function checkPlayer(id){ 
+  
+  //Reading players from database to check if a player already exists
+  await db.collection('Players')
+          .onSnapshot((snapshot) => {
+            var players = snapshot.docs.map((document) => document.data());
+    this.localPlayer = players
+  
+    if(this.localPlayer.length!== 0){     
+      for (var q_id in this.localPlayer){
+      
+        if(id === this.localPlayer[q_id].email ){
+          resultPlayers[0] = this.localPlayer[q_id]
+          qid = resultPlayers[0].q_id 
+          playerExists= true            
+        }
+        //if email id are not equal , creating a new players
+        if(id !== this.localPlayer[q_id].email ){
+          playerExists = false
+        }
+      }  
+    }
+    //First player
+    if(this.localPlayer.length=== 0){   
+      playerExists = false
+    }
+  })
+}
+
+async function addPlayer(){
+    //Adding a player
+    if(playerExists === false){
+      await db.collection("Players").add({email:emailID,q_id:qid})
+      //playerExists = true
+    }      
 }
